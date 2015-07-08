0f5a1ef9761b81436317c3ca7b5c34ae2b123c30
Wilson Tay <taykahhoe89@hotmail.com>
2015-07-08 23:55:45 +0800

    Enable Web Api CORS and Ajax Preflight Request. (Still need to figure out how to remove Global.asax and use only Startup.cs)

--- Refactored/DealsWhat.Application.WebApi/Controllers/CartController.cs
+++ Refactored/DealsWhat.Application.WebApi/Controllers/CartController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace DealsWhat.Application.WebApi.Controllers
 {
@@ -35,6 +36,12 @@ namespace DealsWhat.Application.WebApi.Controllers
         // DELETE api/<controller>/5
         public void Delete(int id)
         {
+        }
+
+        [AllowAnonymous]
+        public HttpResponseMessage Options()
+        {
+            return new HttpResponseMessage { StatusCode = HttpStatusCode.OK };
         }
     }
 }
